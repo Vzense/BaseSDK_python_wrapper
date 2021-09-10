95ee2d70f6f69892eaf86f12a0dd9f8e43ae2fdf
Peter Liu <peter.liu@vzense.com>
2021-09-10 16:16:58 +0800

    modify the bug that do not connet camera will make demo crash, and modify the spelling mistake. thanks @yuki-inahowq

--- zense_cam_demo.py
+++ zense_cam_demo.py
@@ -13,25 +13,28 @@ status = 'init'
 camera_count = camera.get_cam_count()
 
 
-for x in (0, camera_count):
+for x in range(0, camera_count):
     print(str(x) + ':' + str(camera.get_cam_info(x)))
 
 if camera_count > 1:
     index = input("chose open camera:")
-else:
+elif camera_count == 1:
     index = 0
-    print("only one cam, atuo chose")        
-
+    print("only one cam, auto chose")        
+else: 
+    print("there are no camera found")
+    exit()
 
 rst, handle = camera.open_cam(int(index))
 depthrange = DepthRange.NEAR_Range
 depth_max, value_min, value_max = camera.get_measuring_range(handle, 0, depthrange)
+qvga_frame = numpy.zeros((240, 320), dtype = numpy.uint16, order = 'C')
 
 if  rst == 'success':
     print(camera.get_threshold(handle))
     print(camera.get_pulsecnt(handle))
     camera.start_stream(handle)
-    camera.set_data_mode(handle)
+    camera.set_data_mode(handle, 0, DataMode.IR_AND_RGB_30)
     while 1:
         frameready = camera.read_next_frame(handle)
         #print('frame ready:',(frameready.depth, frameready.ir, frameready.rgb))
@@ -50,11 +53,26 @@ if  rst == 'success':
             rst,irframe,width,height = camera.get_frame(handle, 0, FrameType.IR_FRAME)
             if rst == 'success':
                 img = numpy.int32(irframe)
+                intimg = numpy.int32(irframe)
                 img = img*255/4095
                 img = numpy.clip(img, 0, 255)
                 irframe = numpy.uint8(img)
+                for cols in range(0, 320):
+                    for rows in range(0, 240):
+                        mean = intimg[2 * rows, 2 * cols] + intimg[2 * rows, 2 * cols + 1] + intimg[2 * rows + 1, 2 * cols] + intimg[2 * rows + 1, 2 * cols + 1]
+                        qvga_frame[rows, cols] = numpy.uint16(mean/4)
+                qvgaimg = numpy.int32(qvga_frame)
+                qvgaimg = qvgaimg*255/4095
+                qvgaimg = numpy.clip(qvgaimg, 0, 255)
+                qvgaimg = numpy.uint8(qvgaimg)
+                resized_img = cv2.resize(qvgaimg, (640, 480), interpolation = cv2.INTER_LINEAR)
+                cv2.namedWindow("qvairimage")
+                cv2.imshow("qvairimage", qvgaimg)
+                cv2.namedWindow("resized IR")
+                cv2.imshow("resized IR", resized_img)
                 cv2.namedWindow("irimage")
                 cv2.imshow("irimage", irframe)
+                
         if frameready.rgb:
             rst,rgbframe,width,height = camera.get_frame(handle, 0, FrameType.RGB_FRAME)
             if rst == 'success':
@@ -89,6 +107,9 @@ if  rst == 'success':
                 if index == int(mode_input):
                     camera.set_depth_range(handle, 0, element)
                     depth_max, value_min, value_max = camera.get_measuring_range(handle, 0, element)
+        # elif key == ord('s'):
+        #     print("save ir image!!")
+        #     numpy.save("irframe.bin", irframe)
 
             
 
